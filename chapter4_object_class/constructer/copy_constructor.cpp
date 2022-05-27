3be47c34bfd210b550fc0bded698d283089f299d
SamratAryal <78262429+SamratAryal@users.noreply.github.com>
2022-05-27 12:55:59 +0545

    Copy construtor

---
+++ chapter4_object_class/constructer/copy_constructor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+//declare a class
+class wall
+{
+private:
+    /* data */
+    double length;
+    double height;
+public:
+//create prameterized constructor
+wall(double len, double high)
+{
+    length = len;
+    height = high;
+}
+//copy constructor with wall object as parameter
+wall(wall &obj) 
+{
+    length =  obj.length;
+    height = obj.height;
+}
+double calculateArea()
+{
+    return length*height;
+} 
+};
+
+int main()
+{
+    wall w1(20,30);
+    
+    cout<<"The area is: "<<w1.calculateArea()<<endl;
+    return 0;
+}
