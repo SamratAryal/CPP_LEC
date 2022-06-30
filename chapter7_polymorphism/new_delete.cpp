3f6674b6a8d0d2326483717ee0feb3034e67337e
SamratAryal <78262429+SamratAryal@users.noreply.github.com>
2022-06-30 19:43:39 +0545

    allocation of memory on the heap area of the stack

---
+++ chapter7_polymorphism/new_delete.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *pointint;
+    float *pointfloat;
+    pointint=new int;
+    pointfloat=new float;
+    cout<<"Address assigned to pointint="<<pointint<<endl;
+    cout<<"Address assigned to pointfloat="<<pointfloat<<endl;
+//dynamically allocating memory
+    *pointint=50;
+    *pointfloat=50.50;
+
+    cout<<"Value assigned to pointint="<<*pointint<<endl;
+    cout<<"Value assigned to pointfloat="<<*pointfloat<<endl;
+
+    //deallocating memory
+     delete pointint;
+     delete pointfloat;
+}
