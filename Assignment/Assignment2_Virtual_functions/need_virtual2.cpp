93617210e6e83f36959fd988842a6d8ce4561297
SamratAryal <78262429+SamratAryal@users.noreply.github.com>
2022-07-01 19:32:38 +0545

    need of virtual function

---
+++ Assignment/Assignment2_Virtual_functions/need_virtual2.cpp
@@ -0,0 +1,41 @@
+//need of virtual function
+#include<iostream>
+using namespace std;
+class animal
+{
+    public:
+    virtual void display()
+    {
+        cout<<"\nFrom base class animal";
+    }
+};
+class cow : public animal
+{
+    public:
+    void display()
+    {
+        cout<<"\nFrom derived class cow";
+    }
+};
+class dog : public animal
+{
+    public:
+    void display()
+    {
+        cout<<"\nFrom Derived class dog";
+    }
+};
+int main()
+{
+    animal *panm;
+    animal anm;
+    cow cw;
+    dog dg;
+    panm=&anm;
+    panm->display();
+    panm=&cw;
+    panm->display();
+    panm=&dg;
+    panm->display();
+    return 0;
+}
