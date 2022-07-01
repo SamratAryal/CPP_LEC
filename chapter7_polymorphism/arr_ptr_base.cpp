eba12c6726611254f63a966adb9c49213a17b4e1
SamratAryal <78262429+SamratAryal@users.noreply.github.com>
2022-07-01 11:12:19 +0545

    example of array of pointer to base class object

---
+++ chapter7_polymorphism/arr_ptr_base.cpp
@@ -0,0 +1,61 @@
+//example of array of pointer to base class object
+#include<iostream>
+using namespace std;
+
+class point
+{
+    public:
+    virtual void draw()
+    {
+        cout<<"Point"<<endl;
+
+    }
+};
+class line: public point
+{
+    public:
+    void draw()
+    {
+        cout<<"line"<<endl;
+
+    }
+}; 
+class triangle : public point
+{
+    public:
+    void draw()
+    {
+        cout<<"Triangle"<<endl;
+    }
+};
+class polygon: public point
+{
+    public: 
+    void draw()
+    {
+        cout<<"Polygon"<<endl;
+    }
+};
+class circle: public point
+{
+    public: 
+    void draw()
+    {
+        cout<<"circle"<<endl;
+    }
+};
+int main()
+{
+    point pt;
+    line ln;
+    triangle tr;
+    polygon py;
+    circle cr;
+
+    point *baseptr[]={&pt,&ln,&tr,&py,&cr};
+    cout<<"Figure drawn by base pointer are:"<<endl;
+    for(int i=0;i<5;i++)
+    baseptr[i]->draw();
+    return 0;
+
+}
