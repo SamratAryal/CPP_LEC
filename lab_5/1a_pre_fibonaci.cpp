d1afc19b540bf8b81b9ec300593982ac1700679c
SamratAryal <78262429+SamratAryal@users.noreply.github.com>
2022-07-10 20:32:33 +0545

    Fibonacci series using pre increment

---
+++ lab_5/1a_pre_fibonaci.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class fibonacci
+{
+    private:
+    int num1,num2;
+    public:
+    public:
+    fibonacci()
+    {
+        num1=0;
+        num2=1;
+    }
+    void operator ++()
+    {
+        int temp;
+        temp=num1;
+        num1=num1+num2;
+        num2=temp;
+    }
+    void showdata()
+    {
+        cout<<num1<<"\t";
+    }
+};
+int main()
+{
+    fibonacci f;
+    int n,i;
+    cout<<"Enter number of term in fibonacci series:";
+    cin>>n;
+    cout<<"Fibonacci series:"<<endl;
+    cout<<"0"<<"\t";
+    for(i=0;i<n;i++)
+    {
+        ++f;
+        f.showdata();
+    }
+}
