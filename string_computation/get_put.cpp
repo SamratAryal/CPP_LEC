aefc42da79228a7abd19726d1ec538fac60e5b2b
SamratAryal <78262429+SamratAryal@users.noreply.github.com>
2022-07-20 11:26:27 +0545

    use of get and put

---
+++ string_computation/get_put.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char c,str[30];
+cout<<"Enter any character:";
+cin.get(c);
+cout<<"\nThe entered cahracter is:";
+cout.put(c);
+return 0;
+}
